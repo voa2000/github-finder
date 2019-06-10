eb6873c1acc40ec83e0c04e65f59121390146f28
voa2000 <voa2000@hotmail.com>
2019-06-10 13:44:25 +0100

    Create a userItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 
 export class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
