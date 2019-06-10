6ff4cd6306591658d62c89d015c188b6a3f6a2f5
voa2000 <voa2000@hotmail.com>
2019-06-10 13:06:19 +0100

    Added NavBar component & Fontawesome

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+  };
+  render() {
+    return (
+      <div className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
