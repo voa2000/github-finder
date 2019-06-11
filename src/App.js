72953ccb1ecf51f8e6f5b98d166cd63171aaf6a8
voa2000 <voa2000@hotmail.com>
2019-06-11 22:07:57 +0100

    Added search facility to find users by name

--- src/App.js
+++ src/App.js
@@ -10,22 +10,32 @@ class App extends Component {
     users: [],
     loading: false
   };
-  async componentWillMount() {
+  // async componentWillMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=$
+  //     {process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=
+  //     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+  searchUsers = async text => {
     this.setState({ loading: true });
+
     const res = await axios.get(
-      `https://api.github.com/users?client_id=$
+      `https://api.github.com/search/users?q=${text}&client_id=$
       {process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=
       ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
   render() {
     const { loading, users } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={loading} users={users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,25 +1,30 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
     text: ""
   };
-  searchUser = event => {
+  static propTypes = { searchUsers: PropTypes.func.isRequired };
+  changeUser = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
   submitUser = event => {
     event.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
   };
 
   render() {
     return (
       <div>
-        <form className='form' onSubmit={this.submitUser}>
+        <form onSubmit={this.submitUser} className='form'>
           <input
             className='btn btn-dark btn-black'
             type='text'
             name='text'
-            onChange={event => this.searchUser(event)}
+            onChange={event => this.changeUser(event)}
             value={this.state.text}
             placeholder='Search Users ....'
           />
