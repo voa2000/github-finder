bfa9e9b1dbc00fa53323cccd54cf031e862f59f5
voa2000 <voa2000@hotmail.com>
2019-06-18 18:40:17 +0100

    Fetch data about user repo

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -40,13 +41,26 @@ class App extends Component {
     );
     this.setState({ user: res.data, loading: false });
   };
+
+  // Get users repo information
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   //Set alert when input box is empty
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
     setTimeout(() => this.setState({ alert: null }), 5000);
   };
   render() {
-    const { alert, loading, users, user } = this.state;
+    const { alert, loading, users, user, repos } = this.state;
 
     return (
       <Router>
@@ -78,7 +92,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
